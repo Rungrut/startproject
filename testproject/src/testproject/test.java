7501573eeaf5ad7be6b183d7ac5c5c7324496fd1
tylernaja <tylernaja@DESKTOP-6GH60NP>
2019-02-16 14:43:25 -0500

    Main Window, You guys can edit it.

--- testproject/src/testproject/test.java
+++ testproject/src/testproject/test.java
@@ -1,5 +1,36 @@
 package testproject;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.*;
 
 public class test {
-
+	
+	public static void main(String []args)
+	{
+		// ALL UI Components
+		JFrame frame= new JFrame();	
+	
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		frame.getContentPane().add(panel);
+		
+		JLabel label = new JLabel("Test");
+		label.setBounds(500, 500, 500, 60);
+		panel.add(label);		
+		
+		JTextField textfield = new JTextField();
+		textfield.setLocation(700, 700);
+		textfield.setPreferredSize(new Dimension(200,22));
+		panel.add(textfield);
+		
+		JButton buttom = new JButton("buttom");
+		panel.add(buttom);		
+		
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Search Engine Test");
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
 }
